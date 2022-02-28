a89d770925b530df332c45f60612cf892d016b20
skatti3 <katti_sushmitha@optum.com>
2022-03-01 02:39:59 +0530

    add error messages for label

--- CrossOrgFeatures/src/Labels.js
+++ CrossOrgFeatures/src/Labels.js
@@ -1,14 +1,15 @@
 
 
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Card, Accordion, Alert, Badge, Form, Button, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Card, Accordion, Alert, Badge, Form, Button, ListGroup, Table } from 'react-bootstrap';
 
-
+let error_list = []
 function Labels({ userdata, feature, repos, get_api, api, git_api, load_repo_message }) {
 
     const [label, setLabel] = useState({ color: 'fbca04' });
     const [label_method, setLabeleMethod] = useState("CREATE")
     const [color, setColor] = useState("fbca04")
+    const [statuslist, setStatusList] = useState([])
 
     let get_label_number = (labels_list, name) => {
         for (let ll of labels_list) {
@@ -21,7 +22,7 @@ function Labels({ userdata, feature, repos, get_api, api, git_api, load_repo_mes
     }
     let update_label = (label_url, name) => {
         let label_list = api(git_api + label_url, 'GET')
-        label_list.then(list => {
+        let update_respose = label_list.then(list => {
             let ll_url = get_label_number(list, name)
             if (ll_url) {
                 let temp_label = label
@@ -33,22 +34,27 @@ function Labels({ userdata, feature, repos, get_api, api, git_api, load_repo_mes
             }
         }
         )
+        return update_respose
     }
+    
 
     let create_label = (label_url) => {
         let repo_data = api(git_api + label_url, 'POST', label)
+
+        return repo_data
     }
 
     let delete_label = (label_url, name) => {
         let label_list = api(git_api + label_url, 'GET')
 
-        label_list.then(list => {
+        let delete_response = label_list.then(list => {
             let ll_url = get_label_number(list, name)
             if (ll_url) {
                 api(ll_url, 'DELETE')
             }
 
         })
+        return delete_response
     }
 
     let color_generator = () => {
@@ -59,19 +65,117 @@ function Labels({ userdata, feature, repos, get_api, api, git_api, load_repo_mes
     }
 
     const handleSubmitLabel = (evt) => {
+
+        error_list = []
         evt.preventDefault();
         let label_url = "repos/" + userdata.username + "/" + 'portal' + "/labels"
-        repos.map(repo => {
+        console.log("ERROR LIST BEFORE", error_list)
 
-            let label_url = "repos/" + userdata.username + "/" + repo + "/labels"
-            if (label_method == "CREATE")
-                create_label(label_url)
-            else if (label_method == "UPDATE")
-                update_label(label_url, label['name'])
-            else if (label_method == "DELETE")
-                delete_label(label_url, label['name'])
+        let something = function CUDRepos() {
+            return new Promise((resolve, reject) => {
+                Promise.all(repos.map(repo => {
+
+                    let label_url = "repos/" + userdata.username + "/" + repo + "/labels"
+                    if (label_method == "CREATE") {
+
+                        let response = create_label(label_url)
+                        let create_return = response.then(res => {
+                            console.log("CAME TO Success")
+                            error_list.push({ repo_name: repo, status: 0, message: 'Success' })
+                            return
+                        }
+                        )
+                            .catch(error => {
+                                if (error instanceof Promise) {
+                                    error.then(err => {
+
+                                        error_list.push({ repo_name: repo, status: 1, message: err })
+                                        return
+
+                                    })
+                                }
+                                else {
+
+                                    error_list.push({ repo_name: repo, status: 1, message: error })
+                                    return
+
+                                }
+
+                            })
+                        return create_return
+
+                    }
+
+                    else if (label_method == "UPDATE") {
+                        let response = update_label(label_url, label['name'])
+                        let create_return = response.then(res => {
+                            console.log("CAME TO Success")
+                            error_list.push({ repo_name: repo, status: 0, message: 'Success' })
+                            return
+
+                        }
+                        )
+                            .catch(error => {
+                                if (error instanceof Promise) {
+                                    error.then(err => {
+
+                                        error_list.push({ repo_name: repo, status: 1, message: err })
+                                        return
+
+                                    })
+                                }
+                                else {
+
+                                    error_list.push({ repo_name: repo, status: 1, message: error })
+                                    return
+
+                                }
+
+                            })
+                        return create_return
+
+                    }
+                    else if (label_method == "DELETE") {
+                        let response = delete_label(label_url, label['name'])
+                        let create_return = response.then(res => {
+                            console.log("CAME TO Success")
+                            error_list.push({ repo_name: repo, status: 0, message: 'Success' })
+                            return
+
+                        }
+                        )
+                            .catch(error => {
+                                if (error instanceof Promise) {
+                                    error.then(err => {
+
+                                        error_list.push({ repo_name: repo, status: 1, message: err })
+                                        return
+
+                                    })
+                                }
+                                else {
+
+                                    error_list.push({ repo_name: repo, status: 1, message: error })
+                                    return
+
+                                }
+
+                            })
+                        return create_return
+                    }
+
+                }
+
+                )).then(see => resolve("RESOLVED"))
+            })
         }
-        )
+
+        something().then((res) => {
+            setStatusList(error_list)
+            console.log("CUD", res, error_list)
+
+        })
+
     }
 
     const handleLabelInputs = (type, value) => {
@@ -91,62 +195,93 @@ function Labels({ userdata, feature, repos, get_api, api, git_api, load_repo_mes
     return (
         <Row className="g-12" className="justify-content-md-center">
             {feature && feature === "label_feature" ?
-
-                <Col md={4}>
+                <>
                     {repos && repos.length > 0 ?
-                        <Container>
+                        <Col md={4}>
 
-                            <h1> Labels</h1>
+                            <Container>
 
-                            <Form onSubmit={handleSubmitLabel}>
-                                <fieldset>
-                                    <Form.Group className="mb-5">
-                                        <Form.Label htmlFor="disabledTextInput">Name</Form.Label>
-                                        <Form.Control id="label-name" onChange={e => handleLabelInputs('name', e.target.value)} required />
-                                    </Form.Group>
-                                    <Form.Group className="mb-5">
-                                        <Form.Label htmlFor="disabledTextInput">Description</Form.Label>
-                                        <Form.Control as="textarea" id="label-description" rows={3} onChange={e => handleLabelInputs('description', e.target.value)} />
-                                    </Form.Group>
-                                    <Form.Group className="mb-5">
-                                        <Form.Label htmlFor="disabledTextInput">Color</Form.Label>
-                                        <Row>
-                                            <Col md={4}>
-                                                <Form.Control id="color" value={label.color} onChange={e => handleLabelInputs('color', e.target.value)} />
-                                            </Col>
-                                            <Col md={4}>
-                                                <p style={{ backgroundColor: '#' + color }}>{color}</p>
-                                            </Col>
-                                            <Col md={4}>
-                                                <Button onClick={() => color_generator()}>Next color</Button>
-                                            </Col>
-                                        </Row>
+                                <h1> Labels</h1>
+
+                                <Form onSubmit={handleSubmitLabel}>
+                                    <fieldset>
+                                        <Form.Group className="mb-5">
+                                            <Form.Label htmlFor="disabledTextInput">Name</Form.Label>
+                                            <Form.Control id="label-name" onChange={e => handleLabelInputs('name', e.target.value)} required />
+                                        </Form.Group>
+                                        <Form.Group className="mb-5">
+                                            <Form.Label htmlFor="disabledTextInput">Description</Form.Label>
+                                            <Form.Control as="textarea" id="label-description" rows={3} onChange={e => handleLabelInputs('description', e.target.value)} />
+                                        </Form.Group>
+                                        <Form.Group className="mb-5">
+                                            <Form.Label htmlFor="disabledTextInput">Color</Form.Label>
+                                            <Row>
+                                                <Col md={4}>
+                                                    <Form.Control id="color" value={label.color} onChange={e => handleLabelInputs('color', e.target.value)} />
+                                                </Col>
+                                                <Col md={4}>
+                                                    <p style={{ backgroundColor: '#' + color }}>{color}</p>
+                                                </Col>
+                                                <Col md={4}>
+                                                    <Button onClick={() => color_generator()}>Next color</Button>
+                                                </Col>
+                                            </Row>
 
 
-                                    </Form.Group>
-                                    <Form.Group className="mb-5">
-                                        <Form.Label htmlFor="disabledSelect">Operation: </Form.Label>
-                                        <Form.Select id="labelMethod" onChange={e => setLabeleMethod(e.target.value)} >
-                                            <option>CREATE</option>
-                                            <option>UPDATE</option>
-                                            <option>DELETE</option>
-                                        </Form.Select>
-                                    </Form.Group>
-                                    <Form.Group className="mb-5">
-                                        <Form.Label htmlFor="disabledTextInput">Rename</Form.Label>
-                                        <Form.Control id="label-rename" onChange={e => handleLabelInputs('rename', e.target.value)} />
-                                        <Form.Text className="text-muted">
-                                            Fill this when you want to update the name
-                                        </Form.Text>
-                                    </Form.Group>
-                                    <Button style={{ backgroundColor: "#e87722" }} type="submit">Submit</Button>
-                                </fieldset>
-                            </Form>
-                        </Container> : <p style={{ color: "red" }} >{load_repo_message}</p>}
-                </Col> : undefined
+                                        </Form.Group>
+                                        <Form.Group className="mb-5">
+                                            <Form.Label htmlFor="disabledSelect">Operation: </Form.Label>
+                                            <Form.Select id="labelMethod" onChange={e => setLabeleMethod(e.target.value)} >
+                                                <option>CREATE</option>
+                                                <option>UPDATE</option>
+                                                <option>DELETE</option>
+                                            </Form.Select>
+                                        </Form.Group>
+                                        <Form.Group className="mb-5">
+                                            <Form.Label htmlFor="disabledTextInput">Rename</Form.Label>
+                                            <Form.Control id="label-rename" onChange={e => handleLabelInputs('rename', e.target.value)} />
+                                            <Form.Text className="text-muted">
+                                                Fill this when you want to update the name
+                                            </Form.Text>
+                                        </Form.Group>
+                                        <Button style={{ backgroundColor: "#e87722" }} type="submit">Submit</Button>
+                                    </fieldset>
+                                </Form>
+
+                            </Container>
+                            <br />
+
+                            {statuslist && statuslist.length > 0 &&
+
+                                < Table responsive="sm">
+                                    <thead>
+                                        <tr>
+                                            <th>REPO</th>
+                                            <th>STATUS</th>
+                                            <th>MESSAGE</th>
+
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {statuslist.map(error => {
+                                            return <tr>
+                                                <td>{error.repo_name}</td>
+                                                <td>{error.status}</td>
+                                                <td>{error.message}</td>
+                                            </tr>
+                                        })}
+
+                                    </tbody>
+                                </Table>
+
+
+                            }
+
+                        </Col> : <p style={{ color: "red" }} >{load_repo_message}</p>}
+                </> : undefined
             }
 
-        </Row>
+        </Row >
     );
 }
 export default Labels;
--- CrossOrgFeatures/src/Parent.js
+++ CrossOrgFeatures/src/Parent.js
@@ -25,7 +25,7 @@ export default function Parent() {
     }
 
     let load_repositores = () => {
-        let repo_data = get_api(git_api + "users/" + userdata.username + "/repos", 'GET')
+        let repo_data = get_api(git_api + "search/repositories", 'GET')
 
         repo_data.then(data => {
             let repo_list = get_repos(data)
@@ -33,15 +33,47 @@ export default function Parent() {
 
         }
         )
-        
+
     }
 
+
+    // For only get repos, because we are using search. It has different syntax
     let get_api = (url, method, api_data, limit = 30, page = 1) => {
+
+        url = url.split('?')[0] + `?page=${page}&q=user:${userdata.username}`
+        let response = api(url, method, api_data, page)
+        let final_response = response.then(res => {
+            if (res && Object.keys(res).length == limit) {
+
+                let inner_res = get_api(url, method, api_data, limit = 30, page + 1)
+
+                let nested_respose = inner_res.then(inner => {
+
+                    return [...res.items, ...inner.items]
+                }
+
+                )
+                return nested_respose
+            }
+            else {
+
+                return [...res.items]
+            }
+        }
+        ).catch(error => { throw (error) })
+
+        return final_response
+
+    }
+
+    // This is for list of dicts
+
+    let get_list_api = (url, method, api_data, limit = 30, page = 1) => {
 
         url = url.split('?')[0] + `?page=${page}`
         let response = api(url, method, api_data, page)
         let final_response = response.then(res => {
-            if (Object.keys(res).length == limit) {
+            if (res && Object.keys(res).length == limit) {
 
                 let inner_res = get_api(url, method, api_data, limit = 30, page + 1)
 
@@ -58,7 +90,7 @@ export default function Parent() {
                 return [...res]
             }
         }
-        )
+        ).catch(error => { throw (error) })
 
         return final_response
 
@@ -78,17 +110,38 @@ export default function Parent() {
             headers: {
                 'Accept': 'application/vnd.github.inertia-preview+json',
                 "Content-Type": "application/json",
-                "Authorization": 'Basic ' + btoa(`${userdata.username}:${userdata.pat}`)
+                'Authorization': `token ${userdata.pat}`
 
 
             },
             body: JSON.stringify(api_data)
-        }).then(response => response.json())
+        }).then(response => {
+            if (!response.ok) {
+                throw response.json()
+            }
+            return response.json()
+        })
             .then(data => {
-                console.log(data, method)
+                // console.log(data, method)
                 return data
             })
-            .catch(error => console.log(error,))
+            .catch(error => {
+                if (error instanceof Promise) {
+                    
+                    let data = error.then(err => {
+                        // console.log("API ERROR", err)
+                        return "MESSAGE: " +  err['message'] + "Error: " + JSON.stringify(err['errors'])
+                    })
+                    throw (data)
+
+                }
+                else {
+                    // console.log("API_ERROR_NO_PROMISING", error)
+                    throw (error)
+
+                }
+
+            })
         return response
 
 
@@ -139,10 +192,6 @@ export default function Parent() {
                         <Nav.Link id="milestone_feature" onClick={selectfeature} >Milestones</Nav.Link>
                         <Nav.Link id="label_feature" onClick={selectfeature} >Labels</Nav.Link>
                         <Nav.Link id="label_feature" href="https://github.com/paridhi-gupta1998/CrossOrgFeatures" >Github</Nav.Link>
-
-
-
-
                     </Nav>
                 </Container>
 
